e2030afe416702c8a8cfd5ca350f405052ac1b1a
zhendahu <zhendahu@gmail.com>
2022-11-30 00:54:14 -0800

    Update ext2-create.c

--- ext2-create.c
+++ ext2-create.c
@@ -424,7 +424,7 @@ void write_inode_table(int fd) {
 	hello_world_inode.i_ctime = current_time;
 	hello_world_inode.i_dtime = 0;
 	hello_world_inode.i_gid = 1000;
-	hello_world_inode.i_links_count = 1;
+	hello_world_inode.i_links_count = 2;
 	hello_world_inode.i_blocks = 2;
 	hello_world_inode.i_block[0] = HELLO_WORLD_FILE_BLOCKNO;
 	write_inode(fd, HELLO_WORLD_INO, &hello_world_inode);
