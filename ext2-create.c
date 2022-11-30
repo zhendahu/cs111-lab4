bb85c18bad0a47adc1c4102c6ab543233283a8bc
zhendahu <zhendahu@gmail.com>
2022-11-30 01:14:09 -0800

    Update ext2-create.c

--- ext2-create.c
+++ ext2-create.c
@@ -412,13 +412,13 @@ void write_inode_table(int fd) {
 	write_inode(fd, EXT2_ROOT_INO, &root_dir_inode);
 
 	struct ext2_inode hello_world_inode = {0};
-	hello_world_inode.i_mode = EXT2_S_IFDIR
+	hello_world_inode.i_mode = EXT2_S_IFREG
 	                              | EXT2_S_IRUSR
 	                              | EXT2_S_IWUSR
 	                              | EXT2_S_IRGRP
 	                              | EXT2_S_IROTH;
 	hello_world_inode.i_uid = 1000;
-	hello_world_inode.i_size = 1024;
+	hello_world_inode.i_size = 12;
 	hello_world_inode.i_atime = current_time;
 	hello_world_inode.i_ctime = current_time;
 	hello_world_inode.i_ctime = current_time;
