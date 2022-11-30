edc7a84e5fd6a725d0a8437e131038ffea71c4fd
zhendahu <zhendahu@gmail.com>
2022-11-30 00:52:05 -0800

    Update ext2-create.c

--- ext2-create.c
+++ ext2-create.c
@@ -519,6 +519,20 @@ void write_hello_world_file_block(int fd) {
 		errno_exit("lseek");
 	}
 
+	ssize_t bytes_remaining = BLOCK_SIZE;
+
+	struct ext2_dir_entry current_entry = {0};
+	dir_entry_set(current_entry, HELLO_WORLD_INO, ".");
+	dir_entry_write(current_entry, fd);
+
+	bytes_remaining -= current_entry.rec_len;
+
+	struct ext2_dir_entry parent_entry = {0};
+	dir_entry_set(parent_entry, EXT2_ROOT_INO, "..");
+	dir_entry_write(parent_entry, fd);
+
+	bytes_remaining -= parent_entry.rec_len;
+
 	char* str = "Hello world\n";
 	ssize_t size = sizeof(str);
 	if (write(fd, &str, size) != size) {
